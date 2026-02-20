330eccff0b98777d4fbef2b714236a47d170ed25
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2026-02-20 15:48:59 +0800

    Updated data

--- src/gra/data.py
+++ src/gra/data.py
@@ -5,18 +5,36 @@ from gwosc.datasets import find_datasets, event_gps, event_detectors
 from gwosc.locate import get_event_urls
 import gwpy
 import gwpy.timeseries
+import os
 # Typer apps:
 import typer
 
 # Typer apps:
-###
+##############################
 data_app = typer.Typer(help="Manage your data.")
+
 data_get_app = typer.Typer(help="Download data for a specific event.") # Make a sub command 'get' that allows for downloading of either the strain data or the PE samples. Should work as `gra data get strain GW150914` or `gra data get pe GW150914`.
 data_inject_app = typer.Typer(help="Create injection data with either real or simulated noise.")
+data_ls_app = typer.Typer(help="List available data files.")
+
+#data_get_lvk_app = typer.Typer(help="Download data for a specific or all event from the LVK catalogs.")
+#data_get_2mass_app = typer.Typer(help="Download data for a specific or all event from the 2MASS catalog.")
+
+data_inject_lvk_app = typer.Typer(help="Create injection data with either real or simulated noise from the LVK catalogs.")
+
+data_ls_lvk_app = typer.Typer(help="List available data files from the LVK catalogs.")
+
 # Add subcommands to data_app
+# data [get/inject/ls]
 data_app.add_typer(data_get_app, name="get")
 data_app.add_typer(data_inject_app, name="inject")
-###
+data_app.add_typer(data_ls_app, name="ls")
+# data get [lvk/2mass]
+#data_get_app.add_typer(data_get_lvk_app, name="lvk")
+#data_get_app.add_typer(data_get_2mass_app, name="2mass")
+# data inject [lvk]
+data_inject_app.add_typer(data_inject_lvk_app, name="lvk")
+##############################
 
 def remove_duplicates(seq):
     # Each event has multiple versions, so the last bit in the name of '-vX'. We remove it and then remove duplicates.
@@ -29,7 +47,7 @@ def remove_duplicates(seq):
             result.append(item_base)
     return result
 
-def _list_data():
+def _list_lvk_data():
     """ List available data files. """
     catalogs = ['GWTC-1-confident', 
                 'GWTC-2.1-confident', 
@@ -44,23 +62,22 @@ def _list_data():
     return events_all
 
 def check_event_name(event_name):
-    events = _list_data()
+    events = _list_lvk_data()
     if event_name not in events:
         typer.echo(f"Event '{event_name}' not found in available events.")
         raise typer.Exit(code=1)
     return events
 
-@data_app.command("ls")
-def list_data():
+@data_ls_app.command("lvk")
+def list_data_lvk():
     """ List available data files. """
-    events = _list_data()
+    events = _list_lvk_data()
     for event in events:
         typer.echo(event)
     typer.echo(f"\nTotal unique events: {len(events)}")
     return events
 
-@data_get_app.command("strain")
-def get_strain(event_name: str = typer.Argument(..., help="Name of the event to download strain data for")):
+def _get_lvk_strain_individual(event_name):
     events = check_event_name(event_name)
     gps = event_gps(event_name)
     typer.echo(f"Event '{event_name}' found with GPS time {gps}.")
@@ -71,12 +88,28 @@ def get_strain(event_name: str = typer.Argument(..., help="Name of the event to 
     for det in detectors:
         typer.echo(f"Fetching {det} data from {start} to {end}...")
         data[det] = gwpy.timeseries.TimeSeries.fetch_open_data(det, start, end, cache=True)
-        filename = f"{event_name}_{det}_strain.gwf"
+        # Make directory for event if it doesn't exist
+        if not os.path.exists(event_name):
+            os.makedirs(event_name)
+        filename = f"{event_name}/{event_name}_{det}_strain.gwf"
         data[det].write(filename, format='gwf')
         typer.echo(f"Saved strain data to {filename}.")
     return data
 
-@data_get_app.command("pe")
+def _get_lvk_strain_all():
+    events = _list_lvk_data()
+    data_all = {}
+    for event in events:
+        data_all[event] = get_lvk_strain(event)
+    return events, data_all
+
+@data_get_app.command("lvk")
+def get_lvk_strain(event_name: str = typer.Argument(..., help="Name of the event to download strain data for; or 'all' if you want to download for all events")):
+    if event_name == 'all':
+        return _get_lvk_strain_all()
+    else:
+        return _get_lvk_strain_individual(event_name)
+
 def get_pe(event_name: str = typer.Argument(..., help="Name of the event to download PE data for")):
     events = check_event_name(event_name)
     datasets = find_datasets(type='pe', event_name=event_name)
