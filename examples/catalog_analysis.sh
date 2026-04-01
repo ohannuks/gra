a113201010bfa0ae53aaf8c68e3e205475d9ffd8
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2026-04-01 10:41:04 +0800

    added catalog analysis

--- examples/catalog_analysis.sh
+++ examples/catalog_analysis.sh
@@ -1,7 +1,7 @@
 # Get all strain data, and PE samples
 gra data get lvk all 
 # Process an example strain data (noise analysis) - produces PSD and noise model samples
-gra data process GW231123_135430
+gra data process lvk GW231123_135430
 # Do PE on all events: --reuse will reuse PE samples from previous runs, if available, to save time
 gra analyse vanilla all --reuse
 # Do the regular vanilla population analysis
@@ -15,7 +15,7 @@ gra analyse millilensing GW231123_135430 --noise=student-t
 # Do the strong lensing analysis for all events
 gra analyse strong all --posterioroverlap --weedcandidates --bbhpop=vanilla --lenspop=default
 # Now do the population-level analysis; limited to top 20 most likely hypotheses.
-gra population all --top20
+gra population all --top=20
 # Print summary of results
 gra summary all
 
